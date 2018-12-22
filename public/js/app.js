6b6f25f22f19ecb57042004f27c62b00aa9cf3ca
Rudy Sharar <rudy@activelylearn.com>
2018-12-22 00:30:00 -0800

    load scraped quotes btn not working

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,24 @@
-function showQuotes(quote, author) {
-    var quote = $("<li>").addClass("quotes list-group-item");
-    var author = $("<h5>").text(author);
-}
+// function showQuotes(quote, author) {
+//     var quote = $("<li>").addClass("quotes list-group-item");
+//     var author = $("<h5>").text(author);
+// }
 
-function pullQuotes(quotes) {
-    for (var j = 0; j < quotes.length; j++) {
-        var quote = quotes[i].quote;
-        var author = quotes[i].author;
-        showQuotes(quote, author)
-    }
-}
+// function pullQuotes(quotes) {
+//     for (var j = 0; j < quotes.length; j++) {
+//         var quote = quotes[i].quote;
+//         var author = quotes[i].author;
+//         showQuotes(quote, author)
+//     }
+// }
 
 $("#new-quotes-btn").on("click", function () {
     console.log('test');
-    $.get("/scrape", function (data) {
-        var newQuotes = [];
-        for (var i in data) {
-            var quote = data[i];
-            newQuotes.push(quote)
-        }
-        pullQuotes(newQuotes);
-    })
+    // $.get("/scrape", function (data) {
+    //     var newQuotes = [];
+    //     for (var i in data) {
+    //         var quote = data[i];
+    //         newQuotes.push(quote)
+    //     }
+    //     pullQuotes(newQuotes);
+    // })
 })
